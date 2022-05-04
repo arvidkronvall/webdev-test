d1eb58c6c1c3d0eb665f761535a45e71d29c642a
arvidkronvall <arvidkronvall@gmail.com>
2022-05-04 19:11:20 +0200

    few fixes

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ function getCatFromGoServer(){
 }
 
 function verifyFileFormat(url) {
+  //Splits filename at seperator '.' to know if jpg or png
   var splittedString = url.split('.'[0])
   var req = new XMLHttpRequest();
 
   req.open('GET', url, false);
   req.send();
+
+  //Tries to open filename, if status not OK then filename doesn't exist
+  //and we change to png and return whole reformed string. Else return string as it came
   if (req.status!=200){
     console.log("file format is wrong")
     return splittedString[0] + ".png";
@@ -36,14 +40,14 @@ function displayCat(cat) {
   info_div.innerHTML = cat.name + '<br/>' + 'Cuteness: '
   + cat.cutenessLevel + '<br/>'
 
-  //Generating Lives Left as Hearts
-
+  //Generating Lives Left as visual Hearts
   for (let i=0; i<cat.livesLeft; i++){
     var lives = document.createElement('h4')
     lives.innerHTML = '&hearts;'
     info_div.appendChild(lives)
   }
 
+  //Paints out a transparant heart if the cat's suitable for allergy sufferers
   if (cat.allergyInducingFur == false){
     var heart = document.createElement('p')
     heart.innerHTML = '&hearts;'
@@ -61,6 +65,7 @@ function generateCats(cats){
   }
 }
 
+//Bubble-Sorting (low->high) Cats and Create a new sorted version as Session Item
 function sortCatsInSessionStorage() {
   let cats = JSON.parse(sessionStorage.getItem('Cats'))
   
@@ -73,7 +78,6 @@ function sortCatsInSessionStorage() {
 			}
 		}
 	}
-  console.log("Creating Sorted List")
   sessionStorage.setItem('Sorted', JSON.stringify(cats))
 }
 
