94a0aea28d185f0d12f747e9df027a9aa84dbd81
arvidkronvall <arvidkronvall@gmail.com>
2022-05-04 18:58:53 +0200

    with '#' will be ignored, and an empty message aborts the commit
    :wq

---
+++ index.js
@@ -0,0 +1,120 @@
+
+//fetches cats from Go Web Server
+function getCatFromGoServer(){
+  const url = "/cats"
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    sessionStorage.setItem('Cats', JSON.stringify(jsonResponse.cats));
+  });
+}
+
+function verifyFileFormat(url) {
+  var splittedString = url.split('.'[0])
+  var req = new XMLHttpRequest();
+
+  req.open('GET', url, false);
+  req.send();
+  if (req.status!=200){
+    console.log("file format is wrong")
+    return splittedString[0] + ".png";
+  }
+  else return url;
+}
+
+function displayCat(cat) {
+  var _li = document.createElement("li");
+  var _img = document.createElement('img')
+  var info_div = document.createElement("div");
+
+  // Checks that image format is correct, and makes it correct if not
+  _img.src = verifyFileFormat('images/' + cat.image)
+  _li.appendChild(_img);
+
+  info_div.innerHTML = cat.name + '<br/>' + 'Cuteness: '
+  + cat.cutenessLevel + '<br/>'
+
+  //Generating Lives Left as Hearts
+
+  for (let i=0; i<cat.livesLeft; i++){
+    var lives = document.createElement('h4')
+    lives.innerHTML = '&hearts;'
+    info_div.appendChild(lives)
+  }
+
+  if (cat.allergyInducingFur == false){
+    var heart = document.createElement('p')
+    heart.innerHTML = '&hearts;'
+    _li.appendChild(heart)
+  }
+
+  _li.appendChild(info_div);
+  document.getElementById("cat-list").appendChild(_li);
+}
+
+function generateCats(cats){
+  document.getElementById('cat-list').innerHTML = "";
+  for (let i=0; i < cats.length; i++){
+    displayCat(cats[i])
+  }
+}
+
+function sortCatsInSessionStorage() {
+  let cats = JSON.parse(sessionStorage.getItem('Cats'))
+  
+	for (let i = 0; i < cats.length; i++) {
+		for (let j = 0; j < cats.length-1; j++) {
+			if (cats[j].cutenessLevel > cats[j + 1].cutenessLevel) {
+				let temp = cats[j];
+				cats[j] = cats[j + 1];
+				cats[j + 1] = temp;
+			}
+		}
+	}
+  console.log("Creating Sorted List")
+  sessionStorage.setItem('Sorted', JSON.stringify(cats))
+}
+
+//Loads Cats From Go Web Server To Local Storage
+// create array from local storage
+getCatFromGoServer()
+let cats = JSON.parse(sessionStorage.getItem('Cats'))
+generateCats(cats)
+
+sortCatsInSessionStorage()
+let sorted_cats = JSON.parse(sessionStorage.getItem('Sorted'))
+
+//Listener for "Not Cute"-button
+document.getElementById("ascend").addEventListener("click", function() {
+  document.getElementById('cat-list').innerHTML = "";
+
+  //loop from high index to low
+  for (let i = 0; i < sorted_cats.length; i++){
+    displayCat(sorted_cats[i]);
+  }
+
+}, true);
+
+//listener for "Much Cute - Button"
+document.getElementById("descend").addEventListener("click", function() {
+  document.getElementById('cat-list').innerHTML = "";
+  
+  //loop from low index to high
+  for (let i = sorted_cats.length-1; i >= 0; i--){
+    displayCat(sorted_cats[i]);
+  }
+
+}, true);
+
+
+//Listener for None-Button
+document.getElementById("unsorted").addEventListener("click", function() {
+  document.getElementById('cat-list').innerHTML = "";
+  generateCats(cats)
+}, true);
+
+
+
+
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type CatData struct {
+	CatData []Cat `json:"cats"`
+}
+
+type Cat struct {
+	Name               string `json:"name"`
+	Image              string `json:"image"`
+	CutenessLevel      int    `json:"cutenessLevel"`
+	AllergyInducingFur bool   `json:"allergyInducingFur"`
+	LivesLeft          int    `json:"livesLeft"`
+}
+
+//Listens for "GET" and writes in JSON format once received
+func Cats(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		cats := readJsonFile()
+		json.NewEncoder(w).Encode(cats)
+	}
+}
+
+//makes sure we stay at /html.client
+func Home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/cats", Cats)
+	http.HandleFunc("/", Home)
+
+	//Listens to the port that we're hosting our client on
+	//Fatal to receive error messages
+	log.Fatal(http.ListenAndServe(":5500", nil))
+}
+
+//Reads local file and return data as struct
+func readJsonFile() CatData {
+	file, _ := ioutil.ReadFile("catdata.json")
+	data := CatData{}
+
+	//Convert json to struct
+	_ = json.Unmarshal([]byte(file), &data)
+	return data
+}
